2a2029bc1027d8183347367a10924d0c3e0512c2
Gust-Dev <dev.contato.luiz@gmail.com>
2024-12-16 16:31:48 -0300

    isso me da raiva pprt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Atualização de codigo 
 
 for (let i = 0; i<10; i++) {
-    console.log ("voce tem " + i + " Reais em sua carteira do banco")
+    console.log ("voce tem " + i + " Reais em sua carteira do banco nessa merda")
 }
 
